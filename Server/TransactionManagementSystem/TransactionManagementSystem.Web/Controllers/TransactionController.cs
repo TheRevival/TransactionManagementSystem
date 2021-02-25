83887ee81d2d8839c72e1adc2267c5ac4fa801b6
binary robot <vitalii.revival@gmail.com>
2021-02-25 13:50:53 +0200

    fix: fixed connection string, add transaction controller to debug itself through Swagger.

--- Server/TransactionManagementSystem/TransactionManagementSystem.Web/Controllers/TransactionController.cs
+++ Server/TransactionManagementSystem/TransactionManagementSystem.Web/Controllers/TransactionController.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TransactionManagementSystem.Data.Models;
+using TransactionManagementSystem.Service.Interfaces;
+
 namespace TransactionManagementSystem.Web.Controllers
 {
-    public class TransactionController
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController : ControllerBase
     {
-        
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Transaction>> Get()
+        {
+            var transactions = await _transactionService.GetAllTransactions();
+            
+            return transactions;
+        }
     }
 }
--- Server/TransactionManagementSystem/TransactionManagementSystem.Web/Startup.cs
+++ Server/TransactionManagementSystem/TransactionManagementSystem.Web/Startup.cs
@@ -6,24 +6,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TransactionManagementSystem.Data;
 
 namespace TransactionManagementSystem.Web
 {
     public class Startup
-    {
+    {        
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -31,9 +31,12 @@ namespace TransactionManagementSystem.Web
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "TransactionManagementSystem.Web", Version = "v1"});
             });
+            services.AddDbContext<TransactionManagementSystemDbContext>(opts =>
+            {
+                opts.EnableDetailedErrors();
+                opts.UseNpgsql(Configuration.GetConnectionString(""));
+            });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
